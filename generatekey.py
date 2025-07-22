57ef5214f99d6439b337c399078c5b9aa4fdadcc
irakhare67 <iranehal@gmail.com>
2025-07-22 10:08:00 +0530

    Update generatekey.py

--- generatekey.py
+++ generatekey.py
@@ -1,8 +1,8 @@
 import pickle
 from pathlib import Path
 import streamlit_authenticator as stauth
-names = ["Aniket Bhattacharya","Pranav Chavare","Karan Chawla"]
-usernames=["aniket","pranav","karan"]
+names = ["Ira Khare","Pranav Chavare","Karan Chawla"]
+usernames=["ira","pranav","karan"]
 passwords=["12345","12345","12345"]
 hashed_passwords=stauth.Hasher(passwords).generate()
 file_path=Path(__file__).parent / "hashed_pw.pkl"
